eb536bd3da211aeae71ffad2e9bacde0c4514cf9
Sesh-Gallagher <t.ntwana@gmail.com>
2023-12-06 05:16:57 -0800

    multiple - tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
 
     new_tuple = ()
     if len(sentence) == 0:
-        new_tuple = 0, None
+        new_tuple = 0, "None"
     else:
         new_tuple = len(sentence), sentence[0]
 
