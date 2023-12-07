f7a1410f3715de9737e4114907b3e57c9f1f7caa
Sesh-Gallagher <t.ntwana@gmail.com>
2023-12-07 07:10:00 -0800

    easy_print

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
+        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    b = int(sys.argv[3])
+    a = int(sys.argv[1])
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
