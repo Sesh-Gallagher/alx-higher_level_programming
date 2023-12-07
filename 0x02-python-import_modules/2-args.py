79e223f3bf6dfde23c6835cda0e016d9b95582ed
Sesh-Gallagher <t.ntwana@gmail.com>
2023-12-06 19:06:13 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     import sys
 
     num_count = len(sys.argv) - 1
-    if num_count == 1:
-        print("1 argument.")
-    elif num_count == 0:
-        print("0 arguments:")
+    if num_count == 0:
+        print("0 arguments.")
+    elif num_count == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(num_count))
     for a in range(num_count):
