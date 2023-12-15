c74a3cdff7c52cbef86378f1e9aee69ddef5667d
Sesh-Gallagher <t.ntwana@gmail.com>
2023-12-14 21:26:49 -0800

    weight avg

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+
+    if not my_list:
+        return 0
+
+        average = 0
+        weight = 0
+        for tup in my_list:
+            average += tup[0] * tup[1]
+            weight += tup[1]
+
+            return float(average / weight)
