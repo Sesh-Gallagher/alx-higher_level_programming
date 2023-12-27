479758305194a743146cec2dc814ae0815c6a8ba
Sesh-Gallagher <t.ntwana@gmail.com>
2023-12-27 07:10:12 -0800

    classes and objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Square class to be defined here"""
 
+
 class Square:
     """Representation of function called _init__"""
 
-    def __init__(self,size):
-        """ initializes size of new Square self"""
+    def __init__(self, size):
+        """initializes size of new Square self"""
         self.__size = size
