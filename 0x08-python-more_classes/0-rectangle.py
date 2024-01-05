349fea864fc3f537e042ee663fa619dfe2338c7b
Sesh-Gallagher <t.ntwana@gmail.com>
2024-01-04 18:43:33 -0800

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """ A empty class to represent a rectangle."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This readme contains information on all the files contained in this folder
