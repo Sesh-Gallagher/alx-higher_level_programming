a9a5b27e3ea6b03b9011f17ec5706769371e4f72
Sesh-Gallagher <t.ntwana@gmail.com>
2024-01-10 08:59:40 -0800

    JSON STR TO OBJ

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a JSON-to-object function."""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return the an object (python Data Structure)
+    represented by a JSON string.
+
+    Args:
+        my_str: JSON string to represent
+
+    Returns: Object 
+    """
+    return json.loads(my_str)
