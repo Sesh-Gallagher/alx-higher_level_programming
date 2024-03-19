189de1c8fbb4e46d4a132cd1763a1b6a8edd99e8
root <t.ntwana@gmail.com>
2024-03-19 06:50:33 -0700

    Everything object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(a=[]):
-    a += ["Bestschool"]
-    return (", ".join(a))
+def magic_string():
+    magic_string.a = getattr(magic_string, 'a', 0) + 1
+    return ("BestSchool, " * (magic_string.a - 1) + "BestSchool")
