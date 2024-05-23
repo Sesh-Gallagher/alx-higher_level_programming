12a416e6aefa1e74616f40a4777ae2ed75b9640c
Sesh-Gallagher <t.ntwana@gmail.com>
2024-05-22 19:15:00 -0700

    More queries fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.i
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Record miust be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         INNER JOIN `tv_show_genres` AS g
