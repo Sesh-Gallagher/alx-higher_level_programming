07cdaac38668dc914b4955ca0eabc72346880d7b
Sesh-Gallagher <Sesh-Gallagher>
2024-06-20 07:20:49 -0700

    objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,17 @@
 #!/usr/bin/node
-class Rectangle {
-	constructor = (w, h) {
-		this.width =  w;
-		this.height = h;
-	}
-}
-module.exports = Rectangle;
+/**
+ *Write a class Rectangle that defines a rectangle:
+
+You must use the class notation for defining your class
+The constructor must take 2 arguments w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h
+
+*/
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
