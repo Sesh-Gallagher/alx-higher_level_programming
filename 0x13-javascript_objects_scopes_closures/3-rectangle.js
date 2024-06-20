ef19a14004ab1a5e83dbe224a563e255ae4bde19
Sesh-Gallagher <Sesh-Gallagher>
2024-06-20 07:33:24 -0700

    objects_scopes_closures fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,34 @@
 #!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle:
 
-class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
-}
-print () {
-	for  (let a = 0; a < this.height; i++) {
-		let pattern = '';
-		for (let b = 0; b < this.width; b++) {
-			pattern += 'x';
-		}
-		console.log(pattern);
-	}
-}
-}
+You must use the class notation for defining your class
+The constructor must take 2 arguments: w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h
+If w or h is equal to 0 or not a positive integer, create an empty object
+Create an instance method called print() that prints the rectangle using the character X
+*/
 
-module.exports =  Rectangle;
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let a = 0; a < this.height; ++a) {
+      let b = 0;
+
+      for (; b < this.width; ++b) {
+        process.stdout.write('X');
+      }
+
+      if (b === this.width) {
+        console.log('');
+      }
+    }
+  }
+};
