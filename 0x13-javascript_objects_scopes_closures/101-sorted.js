5cc78c9d34116386acbd9c95a407230d2786dec0
Sesh-Gallagher <Sesh-Gallagher>
2024-06-20 19:13:36 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 /*
-Write a script that imports a dictionary of occurrences by user id and computes  dictionary of user ids by occurrence.
-
-Your script must import dict from the file 101-data.js
-In the new dictionary:
-A key is a number of occurrences
-A value is the list of user ids
-Print the new dictionary at the end
+Write a script that imports a dictionary of occurrences by user id and computes  dictionary
 */
 
 const dict = require('./101-data').dict;
+
 const newDict = {};
 
 Object.keys(dict).map(function (key) {
