6a0141a54f620c69cf0f71b7bf175ad59aa3c3b9
Sesh-Gallagher <Sesh-Gallagher>
2024-06-20 19:07:34 -0700

    _objects_scopes_closures fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -7,9 +7,9 @@ In the new dictionary:
 A key is a number of occurrences
 A value is the list of user ids
 Print the new dictionary at the end
-
 */
-const dict = require('./101-data.js').dict;
+
+const dict = require('./101-data').dict;
 const newDict = {};
 
 Object.keys(dict).map(function (key) {
