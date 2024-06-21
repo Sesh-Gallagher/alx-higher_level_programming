04f8cdb9ec5a1f0e94301eb566cf84c081a3dc78
Sesh-Gallagher <Sesh-Gallagher>
2024-06-20 18:52:57 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
+/*
+Write a function that converts a number from base 10 to another base passed as argument:
+
+Prototype: exports.converter = function (base)
+You are not allowed to import any file
+You are not allowed to declare any new variable (var, let, etc..)
+*/
 
 exports.converter = function (base) {
-	function myConverter (n) {
-		return n.toString(base);
-	}
+  function myConverter (n) {
+    return n.toString(base);
+  }
 
-	return myConverter;
+  return myConverter;
 };
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
+/*
+Write a function that prints the number of arguments already printed and the new argument value. (see example below)
+
+Prototype: exports.logMe = function (item)
+Output format: <number arguments already printed>: <current argument value>
+*/
 
 let counter = 0;
 
-exports.logMe =  function count (item) {
-	console.log('${counter}: ${item}');
-	counter += 1;
+exports.logMe = function count (item) {
+  console.log(`${counter}: ${item}`);
+  counter += 1;
 };
