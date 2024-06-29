00584b148333563f51f0606489dd7730dc0610e6
Sesh-Gallagher <t.ntwana@gmail.com>
2024-06-29 03:50:35 -0700

    fix 14 & 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Module that creates the State "California" with the City "San Francisco" from a DB
-"""
+"""Module that creates the State "California"
+with the City "San Francisco" from a DB."""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -10,6 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
