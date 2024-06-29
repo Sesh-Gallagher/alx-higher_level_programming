c523caeaa1be12e4d89ed1b23b74d6ff154eb6e6
Sesh-Gallagher <t.ntwana@gmail.com>
2024-06-29 02:31:19 -0700

    script that changes the name of a State object from the database

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Module that prints the State object with the name passed as argument from the database
-"""
+"""Module that prints the State object with the name passed
+    as argument from the database."""
 
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-import sys
 from model_state import Base, State
+import sys
 
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
