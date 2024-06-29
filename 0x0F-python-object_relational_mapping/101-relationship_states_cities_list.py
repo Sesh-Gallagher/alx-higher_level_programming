8190a8dbadc99bd50402b6b36b17f4153547a347
Sesh-Gallagher <t.ntwana@gmail.com>
2024-06-29 03:56:09 -0700

    fix 15 & 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Module that prints the State object with the name passed as argument from the database
-"""
+""" Module that prints the State object with the name
+passed as argument from the database"""
 
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -11,6 +11,7 @@ from relationship_city import City
 
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
